739c8c151489d9a774ad71df4171901f43a1bbed
hieukle <lehieuhieu202@gmail.com>
2025-04-06 23:00:35 +0700

    ab

--- src/MsrcSecurityUpdates/Private/Set-GlobalVariables.ps1
+++
@@ -1,6 +0,0 @@
-﻿# we also set other shared variables
-$global:msrcApiUrl     = 'https://api.msrc.microsoft.com/cvrf/v3.0'
-Write-Verbose -Message "Successfully defined a msrcApiUrl global variable that points to $($global:msrcApiUrl)"
-
-$global:msrcApiVersion = 'api-version=2023-11-01'
-Write-Verbose -Message "Successfully defined a msrcApiVersion global variable that points to $($global:msrcApiVersion)"
